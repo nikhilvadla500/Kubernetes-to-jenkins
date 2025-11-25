101acad5f7fb9fa91f3fc047e2073cc83ac592ae
Nikhil <nikhilvadla500@gmail.com>
2025-11-25 22:41:45 +0530

    modified to jenkins

---
+++ main.tf
@@ -0,0 +1,37 @@
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "gke-network112"
+  auto_create_subnetworks = true
+}
+
+
+resource "google_container_cluster" "my_cluster" {
+  name     = "my-cluster"
+  location = "us-central1-a"
+
+  remove_default_node_pool = true
+  deletion_protection      = false
+  initial_node_count       = 1
+
+  network    = google_compute_network.vpc_network.name
+
+  
+  logging_service    = "logging.googleapis.com/kubernetes"
+  monitoring_service = "monitoring.googleapis.com/kubernetes"
+}
+
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  location   = "us-central1-a"
+  cluster    = google_container_cluster.my_cluster.name
+  node_count = 2
+
+  node_config {
+    machine_type = "e2-medium"
+    disk_type    = "pd-standard"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "google" {
+    project = "careful-ensign-470212-v5"
+    region = "us-central1"
+    zone = "us-central1-a"
+    credentials = file("C:/Users/Nikhil kumar/Downloads/careful-ensign-470212-v5-2a083d5f1a44.json")
+}
